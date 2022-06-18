c1f92e5aaa5eca7bef67178f183753b7237ae7b2
thanostsiar <thanososfp.tt@gmail.com>
2022-06-18 15:34:07 +0300

    Minor changes

--- carsharing/Models/Renter.cs
+++ carsharing/Models/Renter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace carsharing.Models
 {
@@ -10,14 +12,23 @@ namespace carsharing.Models
             RentedVehicles = new HashSet<RentedVehicle>();
         }
 
+        [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RenterId { get; set; }
+
         public string FirstName { get; set; } = null!;
+
         public string LastName { get; set; } = null!;
+
         public string Email { get; set; } = null!;
+
         public string Phone { get; set; } = null!;
+
         public int Age { get; set; }
+
         public double Experience { get; set; }
+
         public string ProfilePicture { get; set; } = null!;
+
         public string Password { get; set; } = null!;
 
         public virtual ICollection<RentedVehicle> RentedVehicles { get; set; }
