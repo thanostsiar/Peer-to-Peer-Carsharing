c7ec260a1357e783c80a57935a58d35a753ff54a
0x-2FA <2fa_gh@protonmail.com>
2022-06-21 13:51:51 +0300

    Created new Models for the newly created tables in our DB

---
+++ carsharing/Models/PostImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace carsharing.Models
+{
+    public partial class PostImage
+    {
+        public int ImageId { get; set; }
+        public int PostId { get; set; }
+        public string ImageUrl { get; set; } = null!;
+
+        public virtual Post Post { get; set; } = null!;
+    }
+}
---
+++ carsharing/Models/PostThumbnail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace carsharing.Models
+{
+    public partial class PostThumbnail
+    {
+        public int PostId { get; set; }
+        public string ThumbnailUrl { get; set; } = null!;
+
+        public virtual Post Post { get; set; } = null!;
+    }
+}
