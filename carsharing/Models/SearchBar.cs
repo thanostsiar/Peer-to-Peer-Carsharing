d9d4b8f424cc1dbe382e6aa63b0440758a7328b2
0x-2FA <2fa_gh@protonmail.com>
2022-06-21 12:31:44 +0300

    Started creating the Model for the SearchBar

---
+++ carsharing/Models/SearchBar.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace carsharing.Models
+{
+    public class SearchBar
+    {
+        [Required(ErrorMessage = "Please enter ")]
+
+        // TODO: need to check if DateFrom is less than Date.Today
+        // if that's true, then don't accept the request
+        private string DateFrom { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter ")]
+        private string DateTo { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter ")]
+
+        // TODO: need to check if TimeFrom is less than Time.Now
+        // if that's true, then don't accept the request
+        private string TimeFrom { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter ")]
+        private string TimeTo { get; set; } = null!;
+    }
+}
