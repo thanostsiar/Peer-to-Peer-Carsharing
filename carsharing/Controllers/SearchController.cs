68b66a15157c53c48a7f24a4d08602b35fbed060
0x-2FA <2fa_gh@protonmail.com>
2022-06-21 12:33:08 +0300

    Started creating a Controller for the SearchBar

---
+++ carsharing/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using carsharing.Models;
+
+
+namespace carsharing.Controllers
+{
+    public class SearchController : Controller
+    {
+        // get the database context
+        private readonly unipicarsContext _context;
+
+        public SearchController(unipicarsContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Results(SearchBar searchBar)
+        {
+            var listOfPosts = await _context.Posts.ToListAsync();
+
+            return View(listOfPosts);
+
+        }
+    }
+}
