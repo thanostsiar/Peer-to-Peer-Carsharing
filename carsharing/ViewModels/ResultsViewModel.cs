01ce63de8ee7c985655087f3b3c34611a8bc07b6
0x-2FA <2fa_gh@protonmail.com>
2022-06-22 14:29:17 +0300

    Created a new ViewModel for the Result page

---
+++ carsharing/ViewModels/ResultsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using carsharing.Models;
+
+namespace carsharing.ViewModels
+{
+
+    public class ResultsViewModel
+    {
+        public IQueryable<Vehicle> Vehicles { get; set; } = null!;
+
+        public IQueryable<Post> Posts { get; set; } = null!;
+
+        public ResultsViewModel(IQueryable<Vehicle> vehicles, IQueryable<Post> post)
+        {
+            this.Vehicles = vehicles;
+            this.Posts = post;
+        }
+    }
+
+}
