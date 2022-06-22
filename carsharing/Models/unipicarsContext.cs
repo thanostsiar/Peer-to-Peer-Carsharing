813f6edaf03d98f13142586e73e3b55a4ac9817b
0x-2FA <2fa_gh@protonmail.com>
2022-06-22 17:20:00 +0300

    Removed a comment

--- carsharing/Models/unipicarsContext.cs
+++ carsharing/Models/unipicarsContext.cs
@@ -28,7 +28,6 @@ namespace carsharing.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=unipi-cars;Username=unipi;Password=0x2fa;");
             }
         }
