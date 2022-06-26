d5957a0ce882c7664b2da02e8a52f569fbcb0db2
0x-2FA <2fa_gh@protonmail.com>
2022-06-26 18:40:24 +0300

    Added an ErrorMessage

--- carsharing/ViewModels/ResultsViewModel.cs
+++ carsharing/ViewModels/ResultsViewModel.cs
@@ -5,14 +5,17 @@ namespace carsharing.ViewModels
 
     public class ResultsViewModel
     {
+        public String ErrorMessage;
+
         public IQueryable<Vehicle> Vehicles { get; set; } = null!;
 
         public IQueryable<Post> Posts { get; set; } = null!;
 
-        public ResultsViewModel(IQueryable<Vehicle> vehicles, IQueryable<Post> post)
+        public ResultsViewModel(IQueryable<Vehicle> vehicles, IQueryable<Post> post, String errorMessage = "")
         {
             this.Vehicles = vehicles;
             this.Posts = post;
+            this.ErrorMessage = errorMessage;
         }
     }
 
