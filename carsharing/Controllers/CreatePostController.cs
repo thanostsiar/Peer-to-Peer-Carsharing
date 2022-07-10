bef617022d274aeeca807cccfe247f2c2a342f5f
SophiaNikolia <sofia.nikolia@gmail.com>
2022-07-11 00:32:43 +0300

    added asp, made some changes to the controller and added required to the model

--- carsharing/Controllers/CreatePostController.cs
+++ carsharing/Controllers/CreatePostController.cs
@@ -14,8 +14,41 @@ namespace carsharing.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(CreatePost createPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            Post post = new Post();
+            post.Title = createPost.Title;
+            post.Vehicle.Manufacturer = createPost.Manufacturer;
+            post.Vehicle.Model = createPost.Model;
+            post.Vehicle.Year = createPost.Year;
+            post.Vehicle.Type = createPost.Type;
+            post.Vehicle.Color = createPost.Color;
+            post.MaxDaysOfRent = createPost.MaxDaysOfRent;
+            post.Body = createPost.Body;
+
+
+            TempData["Title"] = post.Title;
+            TempData["Manufacturer"] = post.Vehicle.Manufacturer;
+            TempData["Model"] = post.Vehicle.Model;
+            TempData["Year"] = post.Vehicle.Year;
+            TempData["Type"] = post.Vehicle.Type;
+            TempData["Color"] = post.Vehicle.Color;
+            TempData["MaxDaysOfRent"] = post.MaxDaysOfRent;
+            TempData["Body"] = post.Body;
+
             return View("Index");
         }
     }
