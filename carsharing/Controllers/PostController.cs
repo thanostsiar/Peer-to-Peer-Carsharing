75e6887d11fdd1bc57c46e60c583316d298178cb
jasonbarberis <59922675+jasonbarberis@users.noreply.github.com>
2022-07-10 22:56:03 +0300

    Created the Post page

---
+++ carsharing/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using carsharing.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace carsharing.Controllers
+{
+    public class PostController : Controller
+    {
+        // get the database context
+        private unipicarsContext _context;
+
+        // initialize the context
+        public PostController(unipicarsContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Posts.ToList();
+            return View(data[0]);
+        }
+
+    }
+}
