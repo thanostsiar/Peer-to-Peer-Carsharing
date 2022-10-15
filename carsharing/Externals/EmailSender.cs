4e16720bf45a3b62213cf2b15ac00e8ce2b02c54
0x-2FA <2fa_gh@protonmail.com>
2022-10-15 11:38:26 +0300

    Classes for the SMPT server options

---
+++ carsharing/Externals/EmailSender.cs
@@ -0,0 +1,21 @@
+﻿using carsharing.Externals;
+using System.Text.Json;
+
+namespace carsharing.Externals
+{
+    public class EmailSender
+    {
+        public EmailOptions options;
+
+        public EmailSender()
+        {
+            string json = File.ReadAllText("Externals/email-sender.json");
+
+            this.options = JsonSerializer.Deserialize<EmailOptions>(json)!;
+
+            System.Diagnostics.Debug.WriteLine($"port: {options.port}");
+            System.Diagnostics.Debug.WriteLine($"userName: {options.userName}");
+
+        }
+    }
+}
