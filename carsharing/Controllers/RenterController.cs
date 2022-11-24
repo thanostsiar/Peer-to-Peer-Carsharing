2220ce823c10cae5d54674f484ad01ad36b34b0c
0x-2FA <2fa_gh@protonmail.com>
2022-11-24 12:21:21 +0200

    Created a new Controller and a new View for the Become an Owner part

---
+++ carsharing/Controllers/RenterController.cs
@@ -0,0 +1,24 @@
+﻿using carsharing.Areas.Identity.Data;
+using carsharing.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace carsharing.Controllers
+{
+    public class RenterController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<Admin> _logger;
+
+        public RenterController(SignInManager<User> signInManager, ILogger<Admin> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
